def21ff0f2fb948b7671a99821bc73e1c6b04e11
Wakatabi <wakazoku@gmail.com>
2017-05-02 19:24:42 +0900

    test

--- chapter5/date_.php
+++ chapter5/date_.php
@@ -3,7 +3,7 @@
 
 date_default_timezone_set(('Japan'));
 echo '<p>', date('Y/m/d H:i:s'), '</p>';
-echo '<p>', date('Y年m月d日 H時i分s秒'), '</p>';
+echo '<p>', date('Y年m月d日 H時i分s秒!'), '</p>';
 ?>　
 
 <?php require '../footer.php'; ?>
