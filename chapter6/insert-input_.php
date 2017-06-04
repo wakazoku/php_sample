3e99f8d47f0071e66b5c43666ec78661d86ec177
Wakatabi <wakazoku@gmail.com>
2017-06-04 22:45:34 +0900

    写経8日目

---
+++ chapter6/insert-input_.php
@@ -0,0 +1,9 @@
+<?php require '../header.php'; ?>
+<p>商品を追加します。</p>
+<form action="insert-output_.php" method="post">
+    商品名<input type="text" name="name">
+    価格<input type="text" name="price">
+    <input type="submit" value="追加">
+</form>
+
+<?php require '../footer.php'; ?>
