46afeb156d02e1e1574ca428722f75b72c4fa2a9
Wakatabi <t_wakatabi@uluru.jp>
2017-10-25 18:46:41 +0900

    Docker-ComposeでのMySQL永続化

--- index.php
+++ index.php
@@ -5,9 +5,25 @@ $password = $_ENV['MYSQL_ENV_MYSQL_ROOT_PASSWORD'];
  
 $dbh = new PDO($dsn, $user, $password);
   
-$sql = 'SHOW DATABASES;';
-   
-foreach ($dbh->query($sql, PDO::FETCH_ASSOC) as $row) {
-  echo '<p>'.$row['Database'].'</p>';
-}
+$sql = 'select * from test;';
+?>
 
+<table border="1">
+  <tr>
+    <th>id</th>
+    <th>name</th>
+    <th>create_time</th>
+    <th>update_time</th>
+  </tr>
+
+<?php
+foreach ($dbh->query($sql, PDO::FETCH_ASSOC) as $row) {
+  echo '<tr>';
+  echo '<td>'. $row["id"] . '</td>';
+  echo '<td>' . $row["name"] . '</td>';
+  echo '<td>' . $row["create_time"] . '</td>';
+  echo '<td>' . $row["update_time"] . '</td>';
+  echo '</tr>';
+}
+?>
+</table>
