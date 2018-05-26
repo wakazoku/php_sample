89bf3c89fce6e0122c0a0beb9080f1a809a04e21
Wakatabi <wakazoku@gmail.com>
2018-05-26 19:33:35 +0900

    bug: sqlの修正、フォルダ移動、phpinfoの表示

--- app/index.php
+++ app/index.php
@@ -1,1 +1,3 @@
 HelloWorld!
+<?php
+phpinfo();
--- environment/db/db_test.sql
+++ environment/db/db_test.sql
@@ -5,7 +5,7 @@ create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id),
 UNIQUE KEY id_UNIQUE (id)
-) DEFAULT CHARSET=UTF8
+) DEFAULT CHARSET=UTF8;
 
 INSERT INTO app.test (id, name, create_time, update_time) VALUES ('1', 'test', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO app.test (id, name, create_time, update_time) VALUES ('2', 'test', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
